22d3cb795ef8c9492e6ac4a70686cac3dd101c5e
jrson83 <me@jrson.de>
2024-04-24 00:37:56 +0200

    fix(react): fix the remaining type errors

--- packages/react/src/App.ts
+++ packages/react/src/App.ts
@@ -1,22 +1,40 @@
-import { createHeadManager, router } from '@inertiajs-revamped/core'
-import { createElement, useEffect, useMemo, useState } from 'react'
+import {
+  type HeadManager,
+  type Page,
+  type PageProps,
+  createHeadManager,
+  router,
+} from '@inertiajs-revamped/core'
+import {
+  type FunctionComponentElement,
+  type Key,
+  type ProviderProps,
+  type ReactNode,
+  createElement,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 import HeadContext from './HeadContext'
 import PageContext from './PageContext'
+import type { InertiaComponentType, SetupOptions } from './createInertiaApp'
 
-export default function App({
-  // @ts-expect-error
+export default function App<SharedProps extends PageProps = PageProps>({
   children,
-  // @ts-expect-error
   initialPage,
-  // @ts-expect-error
   initialComponent,
-  // @ts-expect-error
   resolveComponent,
-  // @ts-expect-error
   titleCallback,
-  // @ts-expect-error
   onHeadUpdate,
-}) {
+}: {
+  children?: (props: {
+    Component: SetupOptions<SharedProps>['props']['initialComponent']
+    props: Page<SharedProps>['props']
+    key: Key
+  }) => ReactNode
+} & SetupOptions<SharedProps>['props']): FunctionComponentElement<
+  ProviderProps<HeadManager | null>
+> {
   const [current, setCurrent] = useState({
     component: initialComponent || null,
     page: initialPage,
@@ -37,7 +55,7 @@ export default function App({
       resolveComponent,
       swapComponent: async ({ component, page, preserveState }) => {
         setCurrent((current) => ({
-          component,
+          component: component as InertiaComponentType,
           page,
           key: preserveState ? current.key : Date.now(),
         }))
@@ -57,7 +75,6 @@ export default function App({
 
   const renderChildren =
     children ||
-    // @ts-expect-error
     (({ Component, props, key }) => {
       const child = createElement(Component, { key, ...props })
 
@@ -65,16 +82,22 @@ export default function App({
         return Component.layout(child)
       }
 
+      /**
+       * This needs refactoring, because of new `withLayout` feature.
+       *
+       * Somehow React picks up the wrong `createElement` overload when `createElement` is executed inside `reduce`.
+       * But maybe it is because of a wrong overload here or in `createInertiaApp`.
+       *
+       * Usage in Component: `ExampleComponent.layout = [LayoutOne, LayoutTwo, LayoutThree]`
+       */
       if (Array.isArray(Component.layout)) {
-        return (
-          Component.layout
-            .concat(child)
-            .reverse()
+        return Component.layout
+          .concat(child)
+          .reverse()
+          .reduce((children, Layout) =>
             // @ts-expect-error
-            .reduce((children, Layout) =>
-              createElement(Layout, { children, ...props })
-            )
-        )
+            createElement(Layout, { children, ...props })
+          )
       }
 
       return child
