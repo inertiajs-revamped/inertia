0b0380891874617b6c8bc581835c7302879929e8
jrson83 <me@jrson.de>
2024-04-25 02:41:34 +0200

    refactor(vue): restructure exports

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,13 +1,15 @@
 export { router } from '@inertiajs-revamped/core'
-export { usePage } from './app'
+
 export { default as createInertiaApp } from './createInertiaApp'
 export { default as Head } from './head'
 export { default as Link, type InertiaLinkProps } from './link'
-export * from './types'
 export { default as useForm, type InertiaForm } from './useForm'
+export { usePage } from './app'
 export { default as useRemember } from './useRemember'
 
 export { resolvePageComponent } from '@inertiajs-revamped/core'
+
+export * from './types'
 
 export type {
   ActiveVisit,
