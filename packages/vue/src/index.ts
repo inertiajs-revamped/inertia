a716e0070945d284c79a83fc55a891d57daac8a7
jrson83 <me@jrson.de>
2024-04-25 02:38:22 +0200

    fix(vue): re-export all types from core package

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -6,3 +6,32 @@ export { default as Link, type InertiaLinkProps } from './link'
 export * from './types'
 export { default as useForm, type InertiaForm } from './useForm'
 export { default as useRemember } from './useRemember'
+
+export type {
+  ActiveVisit,
+  Component,
+  DefaultPageProps,
+  ErrorBag,
+  Errors,
+  FormDataConvertible,
+  HeadManager,
+  HeadManagerOnUpdate,
+  HeadManagerTitleCallback,
+  InertiaAppResponse,
+  LocationVisit,
+  Method,
+  Modal,
+  Page,
+  PageHandler,
+  PageProps,
+  PageResolver,
+  PendingVisit,
+  PreserveStateOption,
+  Progress,
+  Renderer,
+  RequestPayload,
+  Router,
+  Visit,
+  VisitId,
+  VisitOptions,
+} from '@inertiajs-revamped/core'
