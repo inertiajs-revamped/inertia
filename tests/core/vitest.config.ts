986b2beb631cae9972c32daa6dc521a38ba971b0
jrson83 <me@jrson.de>
2024-05-15 20:08:29 +0200

    chore: fix vitest workspace to import from source

--- tests/core/vitest.config.ts
+++ tests/core/vitest.config.ts
@@ -1,9 +1,18 @@
+import { resolve } from 'node:path'
 import { defineProject } from 'vitest/config'
+
+const alias = (p: string) => resolve(__dirname, p)
 
 // https://github.com/ajv-validator/ajv/issues/2343#issuecomment-2080764965
 
 export default defineProject({
+  resolve: {
+    alias: {
+      '@inertiajs-revamped/core': alias('../../packages/core/src/'),
+    },
+  },
   test: {
+    name: '@inertiajs-revamped/core-test',
     isolate: false,
     mockReset: true,
     restoreMocks: true,
