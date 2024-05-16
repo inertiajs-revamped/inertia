377297b4bb9d84feceaac0f890c230afe97b4182
jrson83 <me@jrson.de>
2024-05-16 20:29:59 +0200

    feat(laravel): add env variables for ssr options, based on (inertia-laravel PR 624)

--- packages/laravel/config/inertia.php
+++ packages/laravel/config/inertia.php
@@ -18,8 +18,8 @@ return [
     |
     */
     'ssr' => [
-        'enabled' => true,
-        'url' => 'http://127.0.0.1:13714',
+        'enabled' => (bool) env('INERTIA_SSR_ENABLED', true),
+        'url' => env('INERTIA_SSR_URL', 'http://127.0.0.1:13714'),
         // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
     ],
 
