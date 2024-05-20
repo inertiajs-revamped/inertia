6cdc23f4996259ce0b2ad192c62bc1ddc8e4a880
jrson83 <me@jrson.de>
2024-05-20 02:54:23 +0200

    fix(presets): install headless only for react preset

--- packages/presets/preset.ts
+++ packages/presets/preset.ts
@@ -567,7 +567,7 @@ async function installNodeDependencies({
         ...(sandbox && { packageManager }),
         packages: [
           // default
-          template === 'breeze' ? '@headlessui/react' : '',
+          template === 'breeze' && ui === 'react' ? '@headlessui/react' : '',
           sandbox
             ? `@inertiajs-revamped/${ui}@workspace:*`
             : `@inertiajs-revamped/${ui}`,
