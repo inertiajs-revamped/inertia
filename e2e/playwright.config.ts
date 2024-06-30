9595e110db11ca91276fc21b06c4b653ece20d64
jrson83 <me@jrson.de>
2024-06-30 03:19:59 +0200

    test(e2e): improve playwright config for ci

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,29 +1,20 @@
 import { defineConfig, devices } from '@playwright/test'
 
+// Base URL to use in actions like `await page.goto('/')`.
 const baseURL = `http://localhost:${process.env.UI === 'react' ? 13714 : 13715}/`
 
 export default defineConfig({
   testDir: './tests',
-  // Run all tests in parallel.
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  //workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  workers: process.env.CI ? 1 : undefined,
+  maxFailures: process.env.CI ? 10 : undefined,
   reporter: [['html', { outputFolder: `report-${process.env.UI || 'react'}` }]],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    // Base URL to use in actions like `await page.goto('/')`.
     baseURL,
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
@@ -40,7 +31,7 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
   ],
-  // Run your local dev server before starting the tests.
+  // todo: refactor server.ts to work dynamic
   webServer: {
     command: `pnpm --filter=e2e-${process.env.UI || 'react'} serve`,
     url: baseURL,
